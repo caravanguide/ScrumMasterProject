9d06da121d070bb93476d77493ed5f2c86ded0c0
Rashidkurban <98862848+caravanguide@users.noreply.github.com>
2022-04-11 10:38:43 -0400

    Added second update

--- src/test/java/helloTest.java
+++ src/test/java/helloTest.java
@@ -1,5 +1,6 @@
 public class helloTest {
     public static void main(String[] args) {
         System.out.println("Hello test");
+        System.out.println("About to share the project on github");
     }
 }
